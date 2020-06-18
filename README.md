959c6a377d2ca7a4aa04978f96ed6a392c37fae4
Dennis Pfisterer <github@farberg.de>
2020-06-18 15:11:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# DFN PKI SOAP Client Test
+
+## Acknowledgement
+
+Libraries used and included in this repository
+- This product includes software developed by DFN-CERT Services GmbH, Hamburg, Germany and its contributors. (cf. [SOAP-Client Version 3.8.1/4.0.2](https://blog.pki.dfn.de/2019/11/soap-client-version-3-8-1-4-0-2/))
+- [kohsuke/args4j](https://github.com/kohsuke/args4j)
+- [Bouncycastle](https://www.bouncycastle.org/)
---
+++ src/SoapClientTest.java
@@ -0,0 +1,56 @@
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+import de.dfncert.soap.DFNCERTPublic;
+import de.dfncert.soap.DFNCERTTypesValidDomain;
+import de.dfncert.tools.*;
+
+public class SoapClientTest {
+	
+	public static void main(String[] args) throws Exception {
+		// Parse command line options
+		CommandLineOptions options = parse(args);
+		
+		// Create client
+		DFNPKIClient client = new DFNPKIClient(options.caName);
+		client.loadRAFromPKCS12(options.p12File, options.password.toCharArray());
+		DFNCERTPublic publicClient = client.getPublic();
+		
+		// Get CA name
+		System.out.println("CA Name: " + client.getCAName());
+		
+		// List valid domains
+		DFNCERTTypesValidDomain[] validDomains = publicClient.getValidDomains(options.raId, "server"); // server or email or "" for all
+		
+		System.out.println("Showing "+ validDomains.length + " valid server domains: ");
+		for(DFNCERTTypesValidDomain d : validDomains)
+			System.out.println("- " + d.getName());
+		
+		
+		System.out.println("Done.");
+	}
+	
+	public static class CommandLineOptions {
+		@Option(name="-ra-id",usage="RA Id")
+		int raId;
+		
+		@Option(name="-p12file")
+	    String p12File;
+	    
+		@Option(name="-password")
+	    String password;
+	    
+		@Option(name="-caname")
+	    String caName = "test-client1-ca";
+	}
+	
+	private static CommandLineOptions parse(String[] args) throws CmdLineException {
+		CommandLineOptions options = new CommandLineOptions();
+		CmdLineParser parser = new CmdLineParser(options);
+		parser.parseArgument(args);
+		return options;
+	} 
+	
+
+}
